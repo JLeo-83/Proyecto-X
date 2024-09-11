6d27c4d7886381bb2b0034d95cdaa87caac57f4f
JLeo-83 <143038770+JLeo-83@users.noreply.github.com>
2024-09-11 12:35:42 -0500

    Entrega final

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,42 @@
+import { Canvas, useFrame } from '@react-three/fiber'
+import {OrbitControls, Sparkles} from "@react-three/drei";
+import { useRef } from "react";
+
+const RotatingCube=()=> {
+  const meshRef = useRef();
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.y += 0.01;
+      meshRef.current.rotation.x += 0.01;
+    }
+  })
+
+  return(
+    <mesh ref={meshRef}>
+      <cylinderGeometry args={[1, 1, 1, 32]} />
+      <meshLambertMaterial color={'#468585'} emissive={'#468585'} />
+
+      <Sparkles count={100} size={6} scale={1} speed={0.002} noise={0.2} color="orange"/>
+    </mesh>
+  ) 
+}
+
+  /**
+   * A canvas component that renders a rotating cube with sparkles.
+   *
+   * @returns {ReactElement} a canvas element with a rotating cube and sparkles
+   */
+const App =()=> {
+  return(
+    <Canvas style={{height:'100vh', width:'100vw', display:'flex', justifyContent:'center', alignItems:'center'}}>
+      <OrbitControls enableZoom enablePan enableRotate/>
+
+      <directionalLight position={[1, 1, 1]} intensity={10} color={'0x9CDBA6'}/>
+      <color attach="background" args={['#F0F0F0']} />
+      <RotatingCube />
+      Hello Three.js
+    </Canvas>    
+  )
+}
+export default App;
